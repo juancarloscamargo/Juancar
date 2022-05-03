02b57dcccdab6b53594e6f8ffe86f916bd121a8b
juancarloscamargo <juancarlos@eprinsa.es>
2022-05-03 07:51:38 +0200

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
